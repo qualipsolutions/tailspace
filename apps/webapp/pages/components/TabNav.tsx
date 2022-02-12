44ee7a7c05edf5191d4bbe8db1b2141a8c70e67b
Tshepo Mgaga <tshepo@qualipsolutions.co.za>
2022-02-12 23:46:36 +0200

    refactor: :recycle:

--- apps/webapp/pages/components/TabNav.tsx
+++ apps/webapp/pages/components/TabNav.tsx
@@ -21,7 +21,21 @@ const TabNav = ({ tabs = [] }: TabNavProps) => (
       <select
         id="tabs"
         name="tabs"
-        className="block w-full focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
+        className="form-select appearance-none
+        block
+        w-full
+        px-3
+        py-1.5
+        text-base
+        font-normal
+        text-gray-700
+        bg-white bg-clip-padding bg-no-repeat
+        border border-solid border-gray-300
+        rounded
+        transition
+        ease-in-out
+        m-0
+        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
         defaultValue={tabs.find((tab) => tab.current)?.name}
       >
         {tabs.map((tab) => (
--- apps/webapp/pages/index.tsx
+++ apps/webapp/pages/index.tsx
@@ -2,19 +2,21 @@ import TabNav from './components/TabNav';
 
 export function Index() {
   return (
-    <div>
-      <TabNav
-        tabs={[
-          { name: 'Details', href: '#pablo', current: true },
-          { name: 'Location', href: '#pablo', current: false },
-          { name: 'Acquisition', href: '#pablo', current: false },
-          { name: 'Valuation', href: '#pablo', current: false },
-          { name: 'Medium', href: '#pablo', current: false },
-          { name: 'Images', href: '#pablo', current: false },
-          { name: 'Documents', href: '#pablo', current: false },
-          { name: 'Reference', href: '#pablo', current: false },
-        ]}
-      />
+    <div className="min-h-full">
+      <div className="max-w-7xl mx-auto px-4 py-4 sm:px-6 lg:px-8">
+        <TabNav
+          tabs={[
+            { name: 'Details', href: '#pablo', current: true },
+            { name: 'Location', href: '#pablo', current: false },
+            { name: 'Acquisition', href: '#pablo', current: false },
+            { name: 'Valuation', href: '#pablo', current: false },
+            { name: 'Medium', href: '#pablo', current: false },
+            { name: 'Images', href: '#pablo', current: false },
+            { name: 'Documents', href: '#pablo', current: false },
+            { name: 'Reference', href: '#pablo', current: false },
+          ]}
+        />
+      </div>
     </div>
   );
 }
